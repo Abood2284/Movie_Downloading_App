515fad310807c798bdb1134216e91462abb23e75
abood <sayyedabood69@gmail.com>
2022-03-01 19:45:15 +0530

    Holds the structure of the app and added some text themes

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import './_home_page.dart';
+import './Movies/movie_provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'DeliMeals',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (ctx) => MovieProvider(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'DeliMeals',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          textTheme: ThemeData.light().textTheme.copyWith(
+                headline6: const TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black),
+                bodyText2: const TextStyle(
+                  fontSize: 18,
+                  color: Colors.grey,
+                ),
+              ),
+        ),
+        home: HomePage(),
       ),
-      home: HomePage(),
     );
   }
 }
